1723edcb788c3a7952eaf97f7b7f99dfdec49579
Baeksnagjun <juny200@naver.com>
2026-08-31 15:41:26 +0900

    end

--- review-service/src/main/java/com/example/reviewservice/client/MemberServiceClient.java
+++ review-service/src/main/java/com/example/reviewservice/client/MemberServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "member-service", url = "http://localhost:8081")
+@FeignClient(name = "member-service")
 public interface MemberServiceClient {
 
     @GetMapping("/api/members/{id}")
